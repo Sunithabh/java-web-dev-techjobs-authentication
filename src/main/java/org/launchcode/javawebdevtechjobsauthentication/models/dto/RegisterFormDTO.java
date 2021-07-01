54f79151eedb7bdbba19506fd64c73099c9103ea
Sunithabh <sunithaprasad.c@gmail.com>
2021-06-30 21:05:12 -0500

    "completed studio 10A"

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/RegisterFormDTO.java
@@ -0,0 +1,17 @@
+package org.launchcode.javawebdevtechjobsauthentication.models.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class RegisterFormDTO extends LoginFormDTO {
+
+   private String verifyPassword;
+
+   public String getVerifyPassword(){
+       return verifyPassword;
+   }
+
+   public void setVerifyPassword(String verifyPassword){
+       this.verifyPassword = verifyPassword;
+   }
+}
--- src/test/java/org/launchcode/javawebdevtechjobspersistent/JavaWebDevTechjobsPersistentApplicationTests.java
+++ src/test/java/org/launchcode/javawebdevtechjobspersistent/JavaWebDevTechjobsPersistentApplicationTests.java
@@ -1,10 +1,11 @@
 package org.launchcode.javawebdevtechjobspersistent;
 
 import org.junit.jupiter.api.Test;
+import org.launchcode.javawebdevtechjobsauthentication.models.data.UserRepository;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class JavaWebDevTechjobsPersistentApplicationTests {
+@SpringBootTest(classes= UserRepository.class)
+class JavaWebDevTechJobsPersistentApplicationTests {
 
 	@Test
 	void contextLoads() {
